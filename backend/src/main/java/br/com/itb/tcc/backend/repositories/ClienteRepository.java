ef39f757d985d9e3a8777ef9b666985e249df2c3
Pablo Junior <pablojuniorgn2@gmail.com>
2022-08-24 18:53:46 -0300

    Delete backend directory

--- backend/src/main/java/br/com/itb/tcc/backend/repositories/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.itb.tcc.backend.repositories;
-
-import br.com.itb.tcc.backend.entities.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
--- backend/src/main/java/br/com/itb/tcc/backend/utils/MessageUtil.java
+++
@@ -1,7 +0,0 @@
-package br.com.itb.tcc.backend.utils;
-
-public class MessageUtil {
-
-    public static final String NOT_FOUND_USER = "User not found";
-
-}
